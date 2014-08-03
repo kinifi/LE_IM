641b2be73f53e10a69b1c83eb83f823596fb5830
Chris Figueroa <chrisfigueroa16@gmail.com>
2014-08-03 00:07:04 -0500

    added right joystick Input for shooting arrows

--- Assets/Scripts/BowandArrow/Quiver.cs
+++ Assets/Scripts/BowandArrow/Quiver.cs
@@ -7,39 +7,48 @@ public class Quiver : MonoBehaviour {
 	public GameObject[] blockbreaker;
 	public Transform getTransform;
 
+	private float RSX, RSY;
+
 	void Update ()
 	{
+		RSX = Input.GetAxis("ShootHorizontal");
+		RSY = Input.GetAxis("ShootVertical");
+
 		if(bow > 0)
 		{
 			Transform shootTransform = getTransform;
 
 			GameObject projectile;
-			if(Input.GetKeyDown(KeyCode.W))
+			if(Input.GetKeyDown(KeyCode.W) || RSY > 0.8)
 			{
+				RSY = 0.0f;
 				projectile = Instantiate(blockbreaker[0], shootTransform.position, Quaternion.identity) as GameObject;
 				projectile.GetComponent<ArrowMover>().ShootUp();
 				bow -= 1;
 				deductArrows(1);
 				//Debug.Log (bow);
 			}
-			if(Input.GetKeyDown(KeyCode.S))
+			if(Input.GetKeyDown(KeyCode.S) || RSY < -0.8)
 			{
+				RSY = 0.0f;
 				projectile = Instantiate(blockbreaker[1], shootTransform.position, Quaternion.identity) as GameObject;
 				projectile.GetComponent<ArrowMover>().ShootDown();
 				bow -= 1;
 				deductArrows(1);
 				//Debug.Log (bow);
 			}
-			if(Input.GetKeyDown(KeyCode.D))
+			if(Input.GetKeyDown(KeyCode.D) || RSX > 0.8)
 			{
+				RSX = 0.0f;
 				projectile = Instantiate(blockbreaker[2], shootTransform.position, Quaternion.identity) as GameObject;
 				projectile.GetComponent<ArrowMover>().ShootRight();
 				bow -= 1;
 				deductArrows(1);
 				//Debug.Log (bow);
   			}
-			if(Input.GetKeyDown(KeyCode.A))
+			if(Input.GetKeyDown(KeyCode.A) || RSX < -0.8)
 			{
+				RSX = 0.0f;
 				projectile = Instantiate(blockbreaker[3], shootTransform.position, Quaternion.identity) as GameObject;
 				projectile.GetComponent<ArrowMover>().ShootLeft();
 				bow -= 1;
