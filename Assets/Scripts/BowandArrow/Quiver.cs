52e2d5210ecc39f731b1508232a2a955c9a3e404
Chris Figueroa <chrisfigueroa16@gmail.com>
2014-08-04 15:30:31 -0500

    changed the input settings

--- Assets/Scripts/BowandArrow/Quiver.cs
+++ Assets/Scripts/BowandArrow/Quiver.cs
@@ -6,56 +6,43 @@ public class Quiver : MonoBehaviour {
 	public int bow = 0;
 	public GameObject[] blockbreaker;
 	public Transform getTransform;
-	private float RSX, RSY;
 	public GameObject[] arrows = new GameObject[10];
-	public float firerate = 1.0f;
-	private float nextfire = 0;
 
 
 	void Update ()
 	{
-		RSX = Input.GetAxis("ShootHorizontal");
-		RSY = Input.GetAxis("ShootVertical");
 
 		if(bow > 0)
 		{
 			Transform shootTransform = getTransform;
-
 			GameObject projectile;
-			if((Time.time>nextfire && Input.GetKeyDown(KeyCode.W)) || (Time.time>nextfire && RSY > -0.98))
+
+			if(Input.GetKeyDown(KeyCode.W) || Input.GetButtonDown("Shooting Up"))
 			{
-				nextfire = Time.time + firerate;
 				projectile = Instantiate(blockbreaker[0], shootTransform.position, Quaternion.identity) as GameObject;
 				projectile.GetComponent<ArrowMover>().ShootUp();
 				bow -= 1;
-				Input.ResetInputAxes();
 				Debug.Log ("Bow: " + bow);
 			}
-			if((Time.time>nextfire && Input.GetKeyDown(KeyCode.S)) || (Time.time>nextfire && RSY < 0.98))
+			if(Input.GetKeyDown(KeyCode.S) || Input.GetButtonDown("Shooting Down"))
 			{
-				nextfire = Time.time + firerate;
 				projectile = Instantiate(blockbreaker[1], shootTransform.position, Quaternion.identity) as GameObject;
 				projectile.GetComponent<ArrowMover>().ShootDown();
 				bow -= 1;
-				Input.ResetInputAxes();
 				Debug.Log ("Bow: " + bow);
 			}
-			if((Time.time>nextfire && Input.GetKeyDown(KeyCode.D)) || (Time.time>nextfire && RSX > 0.98))
+			if(Input.GetKeyDown(KeyCode.D) || Input.GetButtonDown("Shooting Right"))
 			{
-				nextfire = Time.time + firerate;
 				projectile = Instantiate(blockbreaker[2], shootTransform.position, Quaternion.identity) as GameObject;
 				projectile.GetComponent<ArrowMover>().ShootRight();
 				bow -= 1;
-				Input.ResetInputAxes();
 				Debug.Log ("Bow: " + bow);
   			}
-			if((Time.time>nextfire && Input.GetKeyDown(KeyCode.A)) || (Time.time>nextfire && RSX < -0.98))
+			if( Input.GetKeyDown(KeyCode.A) || Input.GetButtonDown("Shooting Left"))
 			{
-				nextfire = Time.time + firerate;
 				projectile = Instantiate(blockbreaker[3], shootTransform.position, Quaternion.identity) as GameObject;
 				projectile.GetComponent<ArrowMover>().ShootLeft();
 				bow -= 1;
-				Input.ResetInputAxes();
 				Debug.Log ("Bow: " + bow);
 			}
 		}
