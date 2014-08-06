2fe220f81c0acb2533eea2f221ad22c3975f0eeb
Chris Figueroa <chrisfigueroa16@gmail.com>
2014-08-05 22:46:34 -0500

    added webcam support

---
+++ Assets/Scripts/TestScripts/testWebcam.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testWebcam : MonoBehaviour {
+
+	public UITexture MyWebcam;
+
+	// Use this for initialization
+	void Start () {
+
+		WebCamTexture webcamTexture = new WebCamTexture();
+		webcamTexture.Play();
+		MyWebcam.GetComponent<UITexture>().mainTexture = webcamTexture;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
