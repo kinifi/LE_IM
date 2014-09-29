52ad9c6ed98e8b34701a3119df6fcae5b4843e23
Chris Figueroa <chrisfigueroa16@gmail.com>
2014-09-28 21:12:40 -0400

    created Alienware Checkbox

---
+++ Assets/Scripts/Alienware/CheckBox.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckBox : MonoBehaviour {
+
+	public GameObject confirmationPanel;
+	public UIToggle alienwareToggle;
+	public GameObject AlienWareObject;
+
+	// Use this for initialization
+	void Start () {
+
+#if !UNITY_STANDALONE_WIN
+		alienwareToggle.enabled = false;
+#endif
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void confirmAlienware()
+	{
+		if(GameObject.Find("_Alien") == null)
+		{
+			GameObject alien;
+			alien = Instantiate(AlienWareObject, new Vector2(0 ,0), Quaternion.identity) as GameObject;
+			alien.name = "_Alien";
+			Debug.Log("Created Alienware Object");
+			HidePopUp();
+		}
+		else
+		{
+			Debug.Log("Object Already Created");
+		}
+
+		HidePopUp();
+	}
+
+	public void HidePopUp()
+	{
+		alienwareToggle.value = false;
+		confirmationPanel.SetActive(false);
+		Debug.Log("Cancel");
+	}
+
+	public void ConfirmPopUp()
+	{
+		if(alienwareToggle.value == true)
+		{
+			confirmationPanel.SetActive(true);
+		}
+		else
+		{
+			HidePopUp();
+			confirmationPanel.SetActive(false);
+		}
+	}
+
+
+}
