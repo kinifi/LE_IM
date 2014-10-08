7af548e1f8f30b763b0bd960b4adb104f40eb386
kinifi <chrisfigueroa16@gmail.com>
2014-10-07 22:12:19 -0400

    Added new options menu

---
+++ Assets/Scripts/ChallengeLevels/FullScreenToggle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FullScreenToggle : MonoBehaviour {
+
+	public UIToggle fullScreenToggle;
+
+	// Use this for initialization
+	void Start () {
+
+		//fullScreenToggle.value = Screen.fullScreen;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void changeToggleValue()
+	{
+		Screen.fullScreen = !Screen.fullScreen;
+		Debug.Log("Toggle FullScreen: " + Screen.fullScreen);
+	}
+}
---
+++ Assets/Scripts/ChallengeLevels/ResolutionToggle.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResolutionToggle : MonoBehaviour {
+
+	public UILabel resolutionLabel;
+	private Resolution[] resolutions;
+	private int ResValue = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+		resolutionLabel.text = Screen.width + "," + Screen.height;
+		getScreenResolutions();
+		Debug.Log(resolutions.Length);
+	}
+
+	private void getScreenResolutions()
+	{
+		resolutions = Screen.resolutions;
+		Debug.Log("Get Screen Resolutions");
+	}
+
+	private void updateLabel()
+	{
+		resolutionLabel.text = resolutions[ResValue].width + "," + resolutions[ResValue].height;
+		//Debug.Log("Update Label value");
+	}
+
+	public void updateButtonLabel()
+	{
+		//Debug.Log("Before:" + ResValue);
+		if(ResValue == resolutions.Length - 1)
+		{
+			ResValue = 0;
+			updateLabel();
+			//Debug.Log("After" + ResValue);
+		}
+		else
+		{
+			ResValue++;
+			updateLabel();
+			//Debug.Log("After" + ResValue);
+		}
+
+	}
+
+
+	public void setResolution()
+	{
+		Screen.SetResolution(resolutions[ResValue].width, resolutions[ResValue].height, Screen.fullScreen);
+	}
+	
+}
