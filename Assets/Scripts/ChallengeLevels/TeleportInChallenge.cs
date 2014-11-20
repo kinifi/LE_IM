194a22d0c625e0869577d45ac2778f286377354c
kinifi <chrisfigueroa16@gmail.com>
2014-11-19 22:25:11 -0500

    changed Textures and fixed teleports

---
+++ Assets/Scripts/ChallengeLevels/TeleportInChallenge.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportInChallenge : MonoBehaviour {
+
+    public GameObject telaOut;
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.position = telaOut.transform.position;
+            audio.Play();
+        }
+    }
+}
