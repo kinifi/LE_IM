15991a3a24915d8c5d517a6d994829ce6c1a83ba
Hollie Buckets <bucketsinmn@gmail.com>
2014-11-22 19:41:58 -0500

    Intro Animated Scene Continued
    Added Guide and Timings are specific

--- Assets/Scripts/IntroScripts/RobbeAnimate.cs
+++ Assets/Scripts/IntroScripts/RobbeAnimate.cs
@@ -3,12 +3,22 @@ using System.Collections;
 
 public class RobbeAnimate : MonoBehaviour {
 
-	//Robbe animation configs
+	//Animation configs
 	Animator anim;
 	Animator polaroidAnim;
+	Animator guideAnim;
+	//Game Object configs
 	private GameObject _robbe;
 	private GameObject _photoPoint;
+
+	//Large Polaroid
+	private SpriteRenderer _largePolaroidSprite;
 	public ParticleSystem _photo;
+
+	//Quick Guide
+	private SpriteRenderer _guideSprite;
+	public ParticleSystem _guide;
+
 
 	//Robbe text configs
 	public GameObject[] textObjects;
@@ -23,17 +33,23 @@ public class RobbeAnimate : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-		//Get Componenets and Objects
+		//Robbe's initialization
 		_robbe = GameObject.Find ("Robbe_Intro");
 		anim = GameObject.Find ("Robbe_Intro").GetComponent<Animator>();
+		//Polaroid initialization
 		polaroidAnim = GameObject.Find ("PolaroidGrow").GetComponent<Animator>();
+		_largePolaroidSprite = GameObject.Find ("PolaroidGrow").GetComponent<SpriteRenderer>();
+		//Guide initialization
+		guideAnim = GameObject.Find ("GuideGrow").GetComponent<Animator>();
+		_guideSprite = GameObject.Find ("GuideGrow").GetComponent<SpriteRenderer>();
+		//Other initalization
 		_photoPoint = GameObject.Find ("PointofPhoto");
 
 		//Upkeep
 		moveRobbe = false;
 
 		//Begin Scene Animations
-		Invoke ("SetRobbe", 1.0f);	
+		Invoke ("StartRobbe", 1.5f);	
 	}
 	
 	// Update is called once per frame
@@ -56,6 +72,7 @@ public class RobbeAnimate : MonoBehaviour {
 			anim.SetBool("PhotoInTarget", false); //Robbe needs to stop running
 
 			anim.SetBool("RobbeToPhoto", true); //Robbe is at the photo and needs to bend
+
 			//Invokes Act 3
 			Invoke ("PolaroidGrow", 1.0f);
 			//prevents this from running again
@@ -63,24 +80,35 @@ public class RobbeAnimate : MonoBehaviour {
 		}
 	}
 
-	//Act1
-	private void SetRobbe ()
+	//Act1 part1
+	private void StartRobbe ()
 	{
 		//Displays first text
 		TurnOnText(textObjects[0]);
-		//Starts the photo particle
+		//Starts the photo and guide particle
 		_photo.Play();
 		Debug.Log ("The photo is falling!");
+		_guide.Play ();
+		Debug.Log ("The guide is falling!");
+		//Invokes Act 1 part 2
+		Invoke("StartRobbeB", 2.5f);
+	}
+
+	//Act1 part2
+	private void StartRobbeB()
+	{
+		//Displays second text
+		TurnOnText(textObjects[1]);
 		//Invokes Act 2
-		Invoke("Polaroid", 4.0f);
+		Invoke("Polaroid", 3.0f);
 	}
 
 	//Act 2 part 1
 	private void Polaroid ()
 	{
 		//Displays the second text
-		TurnOnText(textObjects[1]);
-		//Moves robbe after a time
+		TurnOnText(textObjects[2]);
+		//Moves robbe after a time Invokes Act 2 part 2
 		Invoke ("MoveRobbe", 3.0f);
 	}
 
@@ -89,26 +117,81 @@ public class RobbeAnimate : MonoBehaviour {
 	{
 		//Stops the photo particle
 		_photo.Stop();
+		//Stops the guide particle
+		_guide.Stop();
 		//Starts Robbe's movement controlled in update
 		moveRobbe = true;
 	}
 
-	//Act 3 part 1
+	//Act 3 part 1a
 	private void PolaroidGrow ()
 	{
 		anim.SetBool("RobbeBendToPhoto", true); //Robbe has bent to the photo and needs to return to idel state
 
 		//Grows large polaroid
+		_largePolaroidSprite.enabled = true;
 		polaroidAnim.enabled = true;
 		Debug.Log ("The large polaroid should be growing");
 
 		//Displays the third text
-		TurnOnText(textObjects[2]);
+		TurnOnText(textObjects[3]);
 		//Turns off the large polaroid
-		Invoke ("PolaroidReset", 5.0f);
+		Invoke ("PolaroidGrowB", 2.5f);
 	}
-	//Act 3 part 2
-	private void PolaroidReset ()
+
+	//Act 3 part 1b
+	private void PolaroidGrowB ()
+	{
+		//Displays the fourth text
+		TurnOnText(textObjects[4]);
+		//Invokes GuideGrow Act3 part2
+		Invoke("GuideGrow", 2.5f);
+	}
+
+	//Act 3 part 2a
+	private void GuideGrow ()
+	{
+		//Turns off the large polaroid
+		_largePolaroidSprite.enabled = false;
+		//Grows guide
+		_guideSprite.enabled = true;
+		guideAnim.enabled = true;
+		Debug.Log ("The guide should be growing");
+		
+		//Displays the fifth text
+		TurnOnText(textObjects[5]);
+		//Turns off the large polaroid
+		Invoke ("GuideGrowB", 4.0f);
+	}
+
+	//Act 3 part 2b
+	private void GuideGrowB ()
+	{
+		//Displays the fifth text
+		TurnOnText(textObjects[6]);
+		//Invokes GuideShrink
+		Invoke ("GuideShrink", 4.0f);
+		Debug.Log ("Guide should be shrinking.");
+	}
+
+	private void GuideShrink()
+	{
+		guideAnim.SetBool("TimeToShrink", true);
+		//Invokes turns off the guide
+		Invoke ("GuideReset", 1.0f);
+	}
+
+	//Act 3 part 2c
+	private void GuideReset ()
+	{
+		//Turn off the guide
+		_guideSprite.enabled = false;
+		//Display last text and move robbe off
+		TurnOnText(textObjects[7]);
+		Invoke ("DecideToLeave", 1.75f);
+	}
+	//Moves Robbe out of the scene and starts the campaign level
+	private void DecideToLeave()
 	{
 		moveRobbe = true;
 		//Starts first dungeon
