8aa51fc38144eb5e1c5b02888e844aa4f42be7b5
kinifi <chrisfigueroa16@gmail.com>
2014-12-02 22:28:36 -0500

    Added new stats and achievements to the steam backend call

--- Assets/Scripts/Steam/SteamStatsAndAchievements.cs
+++ Assets/Scripts/Steam/SteamStatsAndAchievements.cs
@@ -9,39 +9,45 @@ using Steamworks;
 class SteamStatsAndAchievements : MonoBehaviour {
 	private enum Achievement : int {
 		Pick_Lock_Pro,
-		Boot_Camp,
 		Falling_Is_Fun,
 		Trampolines_Forever,
-		Story_Start,
-		Memory_Creator,
-		Gen_Dungeon, 
-		Read_Memory, 
-		Keep_Trying, 
-		Base_Jumper, 
+		Gen_Dungeon,
 		Tysjacha,
+		Remain_Strong,
+		Conquer_the_Nightmare,
+		Peace_at_Last,
+		Halfway_There,
+		Full_Tank,
+		Check_the_Memory_Book,
+		Nice_Game_Developers,
 	};
-
+	
 	private Achievement_t[] m_Achievements = new Achievement_t[] {
-
+		
 		new Achievement_t(Achievement.Pick_Lock_Pro,"Pick lock your first door", ""),
 		new Achievement_t(Achievement.Falling_Is_Fun, "Falling into the Darkness", ""),
 		new Achievement_t(Achievement.Trampolines_Forever,"Jump on a Trampoline 100 times", ""),
 		new Achievement_t(Achievement.Gen_Dungeon, "Generate A Dungeon in the Random Generation", ""),
-		new Achievement_t(Achievement.Keep_Trying, "Fall for the 50th Time", ""),
-		new Achievement_t(Achievement.Base_Jumper, "Fall for the 100th Time", ""),
 		new Achievement_t(Achievement.Tysjacha, "Fall for the 1000th Time", ""),
+		new Achievement_t(Achievement.Remain_Strong, "Beat One Boss", ""),
+		new Achievement_t(Achievement.Conquer_the_Nightmare, "Beat Two Bosses", ""),
+		new Achievement_t(Achievement.Peace_at_Last, "Beat Three Bosses", ""),
+		new Achievement_t(Achievement.Halfway_There, "Complete 50% of the Challenge Levels", ""),
+		new Achievement_t(Achievement.Full_Tank, "Complete 100% of the Challenge Levels", ""),
+		new Achievement_t(Achievement.Check_the_Memory_Book, "Complete One Story Page", ""),
+		new Achievement_t(Achievement.Nice_Game_Developers, "Move a Crate. Have an Achievement", ""),
 	};
-
+	
 	// Our GameID
 	private CGameID m_GameID;
-
+	
 	// Did we get the stats from Steam?
 	private bool m_bRequestedStats;
 	private bool m_bStatsValid;
-
+	
 	// Should we store stats this frame?
 	private bool m_bStoreStats;
-
+	
 	// Persisted Stat details
 	public int m_nTotalGamesPlayed;
 	public int m_nTotalNumJumps;
@@ -49,31 +55,32 @@ class SteamStatsAndAchievements : MonoBehaviour {
 	public int m_nTotalNumDeathBySpikes;
 	public int m_nTotalNumDeathByFalling;
 	public int m_nTotalNumHalloweenCandies;
-
+	public int m_nTotalNumBossesDefeated;
+	
 	protected Callback<UserStatsReceived_t> m_UserStatsReceived;
 	protected Callback<UserStatsStored_t> m_UserStatsStored;
 	protected Callback<UserAchievementStored_t> m_UserAchievementStored;
-
+	
 	void OnEnable() {
 		if (!SteamManager.Initialized)
 			return;
-
+		
 		// Cache the GameID for use in the Callbacks
 		m_GameID = new CGameID(SteamUtils.GetAppID());
-
+		
 		m_UserStatsReceived = Callback<UserStatsReceived_t>.Create(OnUserStatsReceived);
 		m_UserStatsStored = Callback<UserStatsStored_t>.Create(OnUserStatsStored);
 		m_UserAchievementStored = Callback<UserAchievementStored_t>.Create(OnAchievementStored);
-
+		
 		// These need to be reset to get the stats upon an Assembly reload in the Editor.
 		m_bRequestedStats = false;
 		m_bStatsValid = false;
 	}
-
+	
 	private void Update() {
 		if (!SteamManager.Initialized)
 			return;
-
+		
 		if (!m_bRequestedStats) {
 			// Is Steam Loaded? if no, can't get stats, done
 			if (!SteamManager.Initialized) {
@@ -83,42 +90,35 @@ class SteamStatsAndAchievements : MonoBehaviour {
 			
 			// If yes, request our stats
 			bool bSuccess = SteamUserStats.RequestCurrentStats();
-
+			
 			// This function should only return false if we weren't logged in, and we already checked that.
 			// But handle it being false again anyway, just ask again later.
 			m_bRequestedStats = bSuccess;
 		}
-
+		
 		if (!m_bStatsValid)
 			return;
-
+		
 		// Get info from sources
-
+		
 		// Evaluate achievements
+		
+		///Check to see if the fall over 15 times stat needs to unlock an achievement
 		foreach (Achievement_t achievement in m_Achievements) {
 			if (achievement.m_bAchieved)
 				continue;
-
+			
 			switch (achievement.m_eAchievementID) {
-				case Achievement.Keep_Trying:
-					if (m_nTotalNumDeathByFalling >= 50) {
-						UnlockAchievement(achievement);
-					}
-					break;
-				case Achievement.Base_Jumper:
-					if (m_nTotalNumDeathByFalling >= 100) {
-						UnlockAchievement(achievement);
-					}
-					break;
-				case Achievement.Tysjacha:
-					if (m_nTotalNumDeathByFalling >= 1000) {
-						UnlockAchievement(achievement);
-					}
-					break;
+			case Achievement.Tysjacha:
+				if (m_nTotalNumDeathByFalling >= 15) {
+					UnlockAchievement(achievement);
+				}
+				break;
 			}
-
+			
 		}
-
+		
+		///Check to see if the number of trampolines achievement should be unlocked
 		foreach (Achievement_t achievement in m_Achievements) {
 			if (achievement.m_bAchieved)
 				continue;
@@ -132,13 +132,45 @@ class SteamStatsAndAchievements : MonoBehaviour {
 			}
 			
 		}
-
-
-
+		
+		/////////////////////////////////////////////////////
+		//////Evaluate the stat and see if the boss achievements
+		//////Should be unlocked
+		/////////////////////////////////////////////////////
+		foreach (Achievement_t achievement in m_Achievements)
+		{
+			if (achievement.m_bAchieved)
+				continue;
+			
+			switch (achievement.m_eAchievementID)
+			{
+			case Achievement.Remain_Strong:
+				if (m_nTotalNumBossesDefeated == 1)
+				{
+					UnlockAchievement(achievement);
+				}
+				break;
+			case Achievement.Conquer_the_Nightmare:
+				if (m_nTotalNumBossesDefeated == 2)
+				{
+					UnlockAchievement(achievement);
+				}
+				break;
+			case Achievement.Peace_at_Last:
+				if (m_nTotalNumBossesDefeated >= 3)
+				{
+					UnlockAchievement(achievement);
+				}
+				break;
+			}
+			
+		}
+		
+		
 		//Store stats in the Steam database if necessary
 		if (m_bStoreStats) {
 			// already set any achievements in UnlockAchievement
-
+			
 			// set stats
 			SteamUserStats.SetStat("NumGames", m_nTotalGamesPlayed);
 			SteamUserStats.SetStat("NumJumps", m_nTotalNumJumps);
@@ -146,7 +178,8 @@ class SteamStatsAndAchievements : MonoBehaviour {
 			SteamUserStats.SetStat("NumDeathsBySpikes", m_nTotalNumDeathBySpikes);
 			SteamUserStats.SetStat("NumDeathsByFalling", m_nTotalNumDeathByFalling);
 			SteamUserStats.SetStat("NumCandiesCollectedHalloween", m_nTotalNumHalloweenCandies);
-
+			SteamUserStats.SetStat("NumBossesDefeated", m_nTotalNumBossesDefeated);
+			
 			bool bSuccess = SteamUserStats.StoreStats();
 			// If this failed, we never sent anything to the server, try
 			// again later.
@@ -160,7 +193,7 @@ class SteamStatsAndAchievements : MonoBehaviour {
 	// public void OnGameStateChange(EClientGameState eNewState) {
 	// 	if (!m_bStatsValid)
 	// 		return;
-
+	
 	// 	switch (eNewState) {
 	// 		case EClientGameState.k_EClientGameActive:
 	// 			// Reset per-game stats
@@ -169,52 +202,90 @@ class SteamStatsAndAchievements : MonoBehaviour {
 	// 			break;
 	// 		case EClientGameState.k_EClientGameWinner:
 	// 			m_nTotalNumWins++;
-
+	
 	// 			// fall through
 	// 			goto case EClientGameState.k_EClientGameDraw;
 	// 		case EClientGameState.k_EClientGameLoser:
 	// 			m_nTotalNumLosses++;
-
+	
 	// 			// fall through
 	// 			goto case EClientGameState.k_EClientGameDraw;
 	// 		case EClientGameState.k_EClientGameDraw:
-
+	
 	// 			// Tally games
 	// 			m_nTotalGamesPlayed++;
-
+	
 	// 			// Accumulate distances
 	// 			m_flTotalFeetTraveled += m_flGameFeetTraveled;
-
+	
 	// 			// New max?
 	// 			if (m_flGameFeetTraveled > m_flMaxFeetTraveled)
 	// 				m_flMaxFeetTraveled = m_flGameFeetTraveled;
-
+	
 	// 			// Calc game duration
 	// 			m_flGameDurationSeconds = Time.time - m_ulTickCountGameStart;
-
+	
 	// 			// We want to update stats the next frame.
 	// 			m_bStoreStats = true;
-
+	
 	// 			break;
 	// 	}
 	// }
-
+	
 	//-----------------------------------------------------------------------------
 	// Purpose: Unlock this achievement
 	//-----------------------------------------------------------------------------
 	private void UnlockAchievement(Achievement_t achievement) {
 		achievement.m_bAchieved = true;
-
+		
 		// the icon may change once it's unlocked
 		//achievement.m_iIconImage = 0;
-
+		
 		// mark it down
 		SteamUserStats.SetAchievement(achievement.m_eAchievementID.ToString());
-
+		
 		// Store stats end of frame
 		m_bStoreStats = true;
 	}
-
+	
+	/// <summary>
+	/// Unlock_s the Nice Game Developers Achievement
+	/// </summary>
+	public void Unlock_Nice_Game_Developers_Achievement()
+	{
+		SteamUserStats.SetAchievement("Nice_Game_Developers");
+		m_bStoreStats = true;
+	}
+	
+	/// <summary>
+	/// Unlock_s the Check the Memory Book Achievement
+	/// </summary>
+	public void Unlock_Check_the_Memory_Book()
+	{
+		SteamUserStats.SetAchievement("Check_the_Memory_Book");
+		m_bStoreStats = true;
+	}
+	
+	/// <summary>
+	/// Unlock_s the full Tank Achievement
+	/// </summary>
+	public void Unlock_Full_Tank_Achievement()
+	{
+		SteamUserStats.SetAchievement("Full_Tank");
+		m_bStoreStats = true;
+	}
+	
+	/// <summary>
+	/// Unlock_s the Halfway There Achievement
+	/// </summary>
+	public void Unlock_Halfway_There_Achievement()
+	{
+		SteamUserStats.SetAchievement("Halfway_There");
+		
+		m_bStoreStats = true;
+	}
+	
+	
 	/// <summary>
 	/// Unlock_s the falling_ is_ fun_ achievement.
 	/// </summary>
@@ -225,7 +296,7 @@ class SteamStatsAndAchievements : MonoBehaviour {
 		//store the stats on the next frame
 		m_bStoreStats = true;
 	}
-
+	
 	/// <summary>
 	/// Unlock_s the gen_ dungeon_ achievement.
 	/// </summary>
@@ -237,19 +308,19 @@ class SteamStatsAndAchievements : MonoBehaviour {
 		//store the stats on the next frame
 		m_bStoreStats = true;
 	}
-
+	
 	/// <summary>
 	/// Unlock_s the pick_ lock_ pro_ achievement.
 	/// </summary>
 	public void Unlock_Pick_Lock_Pro_Achievement () {
-
+		
 		//set the achievement we want to unlock
 		SteamUserStats.SetAchievement("Pick_Lock_Pro");
-
+		
 		//store the stats on the next frame
 		m_bStoreStats = true;
 	}
-
+	
 	//-----------------------------------------------------------------------------
 	// Purpose: We have stats data from Steam. It is authoritative, so update
 	//			our data with those results now.
@@ -257,15 +328,15 @@ class SteamStatsAndAchievements : MonoBehaviour {
 	private void OnUserStatsReceived(UserStatsReceived_t pCallback) {
 		if (!SteamManager.Initialized)
 			return;
-
+		
 		// we may get callbacks for other games' stats arriving, ignore them
 		if ((ulong)m_GameID == pCallback.m_nGameID) {
 			if (EResult.k_EResultOK == pCallback.m_eResult) {
 				Debug.Log("Received stats and achievements from Steam\n");
-
-
+				
+				
 				m_bStatsValid = true;
-
+				
 				// load achievements
 				foreach (Achievement_t ach in m_Achievements) {
 					bool ret = SteamUserStats.GetAchievement(ach.m_eAchievementID.ToString(), out ach.m_bAchieved);
@@ -277,7 +348,7 @@ class SteamStatsAndAchievements : MonoBehaviour {
 						Debug.LogWarning("SteamUserStats.GetAchievement failed for Achievement " + ach.m_eAchievementID + "\nIs it registered in the Steam Partner site?");
 					}
 				}
-
+				
 				// load stats
 				getStats();
 			}
@@ -286,7 +357,7 @@ class SteamStatsAndAchievements : MonoBehaviour {
 			}
 		}
 	}
-
+	
 	/// <summary>
 	/// Sets the stats
 	/// </summary>
@@ -297,20 +368,20 @@ class SteamStatsAndAchievements : MonoBehaviour {
 		SteamUserStats.SetStat("NumDeathsBySpikes", m_nTotalNumDeathBySpikes);
 		SteamUserStats.SetStat("NumDeathsByFalling", m_nTotalNumDeathByFalling);
 		SteamUserStats.SetStat("NumCandiesCollectedHalloween", m_nTotalNumHalloweenCandies);
-
+		SteamUserStats.SetStat("NumBossesDefeated", m_nTotalNumBossesDefeated);
+		
 		bool bSuccess = SteamUserStats.StoreStats();
 		if(bSuccess)
 		{
 			Debug.Log("Success Updating User Stats");
-			//Debug.Log("Spike Stat: " + m_nTotalNumDeathBySpikes);
 		}
 	}
-
+	
 	/// <summary>
 	/// Gets the stats from the server
 	/// </summary>
 	public void getStats() {
-
+		
 		// load stats
 		SteamUserStats.GetStat("NumGames", out m_nTotalGamesPlayed);
 		SteamUserStats.GetStat("NumJumps", out m_nTotalNumJumps);
@@ -318,14 +389,16 @@ class SteamStatsAndAchievements : MonoBehaviour {
 		SteamUserStats.GetStat("NumDeathsBySpikes", out m_nTotalNumDeathBySpikes);
 		SteamUserStats.GetStat("NumDeathsByFalling", out m_nTotalNumDeathByFalling);
 		SteamUserStats.GetStat("NumCandiesCollectedHalloween", out m_nTotalNumHalloweenCandies);
+		SteamUserStats.GetStat("NumBossesDefeated", out m_nTotalNumBossesDefeated);
 		Debug.Log("NumGames: " + m_nTotalGamesPlayed);
 		Debug.Log("NumJumps: " + m_nTotalNumJumps);
 		Debug.Log("NumTrampJumps: " + m_nTotalNumTrampJumps);
 		Debug.Log("NumDeathsBySpikes: " + m_nTotalNumDeathBySpikes);
 		Debug.Log("NumDeathsByFalling: " + m_nTotalNumDeathByFalling);
 		Debug.Log("NumCandiesCollectedHalloween: " + m_nTotalNumHalloweenCandies);
+		Debug.Log("NumBossesDefeated: " + m_nTotalNumBossesDefeated);
 	}
-
+	
 	//-----------------------------------------------------------------------------
 	// Purpose: Our stats data was stored!
 	//-----------------------------------------------------------------------------
@@ -350,7 +423,7 @@ class SteamStatsAndAchievements : MonoBehaviour {
 			}
 		}
 	}
-
+	
 	//-----------------------------------------------------------------------------
 	// Purpose: An achievement was stored
 	//-----------------------------------------------------------------------------
@@ -365,7 +438,7 @@ class SteamStatsAndAchievements : MonoBehaviour {
 			}
 		}
 	}
-
+	
 	// //-----------------------------------------------------------------------------
 	// // Purpose: Display the user's stats and achievements
 	// //-----------------------------------------------------------------------------
@@ -374,7 +447,7 @@ class SteamStatsAndAchievements : MonoBehaviour {
 	// 		GUILayout.Label("Steamworks not Initialized");
 	// 		return;
 	// 	}
-
+	
 	// 	GUILayout.Label("m_ulTickCountGameStart: " + m_ulTickCountGameStart);
 	// 	GUILayout.Label("m_flGameDurationSeconds: " + m_flGameDurationSeconds);
 	// 	GUILayout.Label("m_flGameFeetTraveled: " + m_flGameFeetTraveled);
@@ -385,7 +458,7 @@ class SteamStatsAndAchievements : MonoBehaviour {
 	// 	GUILayout.Label("FeetTraveled: " + m_flTotalFeetTraveled);
 	// 	GUILayout.Label("MaxFeetTraveled: " + m_flMaxFeetTraveled);
 	// 	GUILayout.Label("AverageSpeed: " + m_flAverageSpeed);
-
+	
 	// 	GUILayout.BeginArea(new Rect(Screen.width - 300, 0, 300, 800));
 	// 	foreach(Achievement_t ach in m_Achievements) {
 	// 		GUILayout.Label(ach.m_eAchievementID.ToString());
@@ -393,7 +466,7 @@ class SteamStatsAndAchievements : MonoBehaviour {
 	// 		GUILayout.Label("Achieved: " + ach.m_bAchieved);
 	// 		GUILayout.Space(20);
 	// 	}
-
+	
 	// 	if (GUILayout.Button("RESET STATS AND ACHIEVEMENTS")) {
 	// 		SteamUserStats.ResetAllStats(true);
 	// 		SteamUserStats.RequestCurrentStats();
@@ -401,13 +474,13 @@ class SteamStatsAndAchievements : MonoBehaviour {
 	// 	}
 	// 	GUILayout.EndArea();
 	// }
-
+	
 	private void ResetStatsAndAchievements () {
 		SteamUserStats.ResetAllStats(true);
 		//TODO: Ask if I need to request User stats after resetting them
 		//SteamUserStats.RequestUserStats(SteamUser.GetSteamID());
 	}
-
+	
 	//------------------------------------------------------------------
 	// Use this as a safe way to access the variables need to update stats
 	//-----------------------------------------------------------------
@@ -422,47 +495,53 @@ class SteamStatsAndAchievements : MonoBehaviour {
 			Debug.Log("Games Played" + m_nTotalGamesPlayed);
 		}
 	}
-
+	
 	public void incrementNumOfJumps() {
 		m_nTotalNumJumps++;
 		setStats();
 	}
-
+	
 	public void incrementNumOfTrampJumps() {
 		m_nTotalNumTrampJumps++;
 		setStats();
 	}
-
+	
 	public void incrementNumOfDeathsBySpikes() {
 		m_nTotalNumDeathBySpikes++;
 		setStats();
 	}
-
+	
 	public void incrementNumOfDeathsByFalling() {
 		m_nTotalNumDeathByFalling++;
 		setStats();
 	}
-
+	
 	public void incrementNumOfCandiesCollected()
 	{
 		m_nTotalNumHalloweenCandies++;
 		setStats();
 	}
-
+	
+	public void incrementNumBossesDefeated()
+	{
+		m_nTotalNumBossesDefeated++;
+		setStats();
+	}
+	
 	public void resetSteamData()
 	{
 		ResetStatsAndAchievements();
 		Invoke("setStats", 0.5f);
 	}
-
+	
 	//-----------------------------------------------------------------
-
+	
 	private class Achievement_t {
 		public Achievement m_eAchievementID;
 		public string m_strName;
 		public string m_strDescription;
 		public bool m_bAchieved;
-
+		
 		/// <summary>
 		/// Creates an Achievement. You must also mirror the data provided here in https://partner.steamgames.com/apps/achievements/yourappid
 		/// </summary>
