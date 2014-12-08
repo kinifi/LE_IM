a8a483d0a7482c730acea202d0da7d39ae2b164f
kinifi <chrisfigueroa16@gmail.com>
2014-12-07 21:41:57 -0500

    fixed main menu and added credits screen

---
+++ Assets/Scripts/InputAnyKeyOrMouseLoadLevel.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputAnyKeyOrMouseLoadLevel : MonoBehaviour {
+
+    public string levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            loadLevelNow();
+        }
+        else if(Input.GetMouseButtonDown(1))
+        {
+            loadLevelNow();
+        }
+
+        if(Input.anyKeyDown)
+        {
+            loadLevelNow();
+        }
+
+	
+	}
+
+    public void loadLevelNow()
+    {
+        Application.LoadLevel(levelToLoad);
+    }
+}
