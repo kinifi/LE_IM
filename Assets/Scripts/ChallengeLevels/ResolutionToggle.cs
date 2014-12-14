a9ddfac83b08630e57d354ac631ee8f2dfa107ab
kinifi <chrisfigueroa16@gmail.com>
2014-12-14 16:36:52 -0500

    fixed resolution bugs

--- Assets/Scripts/ChallengeLevels/ResolutionToggle.cs
+++ Assets/Scripts/ChallengeLevels/ResolutionToggle.cs
@@ -2,33 +2,38 @@
 using System.Collections;
 
 public class ResolutionToggle : MonoBehaviour {
-
+	
 	public UILabel resolutionLabel;
 	private Resolution[] resolutions;
 	private int ResValue = 0;
-
+	private bool isDirty = false;
+	
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 		resolutionLabel.text = Screen.width + "," + Screen.height;
 		getScreenResolutions();
 		Debug.Log(resolutions.Length);
+		Debug.Log(">>>>>>Current resolution");
 	}
-
+	
 	private void getScreenResolutions()
 	{
 		resolutions = Screen.resolutions;
 		Debug.Log("Get Screen Resolutions");
 	}
-
+	
 	private void updateLabel()
 	{
 		resolutionLabel.text = resolutions[ResValue].width + "," + resolutions[ResValue].height;
 		//Debug.Log("Update Label value");
 	}
-
+	
 	public void updateButtonLabel()
 	{
+		isDirty = true;
+		
 		//Debug.Log("Before:" + ResValue);
 		if(ResValue == resolutions.Length - 1)
 		{
@@ -42,13 +47,34 @@ public class ResolutionToggle : MonoBehaviour {
 			updateLabel();
 			//Debug.Log("After" + ResValue);
 		}
-
+		
 	}
-
-
+	
+	
 	public void setResolution()
 	{
-		Screen.SetResolution(resolutions[ResValue].width, resolutions[ResValue].height, Screen.fullScreen);
+		if(isDirty)
+		{
+			if(Screen.fullScreen)
+			{
+				Screen.SetResolution(resolutions[ResValue].width, resolutions[ResValue].height, Screen.fullScreen);
+			}
+			else
+			{
+				Screen.SetResolution(resolutions[ResValue].width, resolutions[ResValue].height, !Screen.fullScreen);
+			}
+		}
+		else
+		{
+			if(Screen.fullScreen)
+			{
+				Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, Screen.fullScreen);
+			}
+			else
+			{
+				Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, !Screen.fullScreen);
+			}
+		}
 	}
 	
 }
