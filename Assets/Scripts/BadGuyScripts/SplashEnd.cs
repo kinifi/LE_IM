4bc695c005562ceda3117bbaedc464da7b45d0c5
Hollie Buckets <bucketsinmn@gmail.com>
2015-01-11 16:52:20 -0500

    ShaddowBox_GreenSplash Animation
    First ShaddowBox animation commint :D

---
+++ Assets/Scripts/BadGuyScripts/SplashEnd.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashEnd : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Invoke ("KillMe", 0.55f);
+	
+	}
+	
+	public void KillMe ()
+	{
+		Destroy(this.gameObject);
+	}
+}
