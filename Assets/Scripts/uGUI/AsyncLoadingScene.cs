7264cca9be67439eeb66fcf65b51113fc16f4450
kinifi <chrisfigueroa16@gmail.com>
2015-02-01 22:20:01 -0500

    asynclevelload call made

---
+++ Assets/Scripts/uGUI/AsyncLoadingScene.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsyncLoadingScene : MonoBehaviour {
+
+	private string levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+	
+		asyncLevelToLoad("Tutorial_Dungeon");
+
+		if(PlayerPrefs.HasKey("asynclevel"))
+	    {
+			levelToLoad = PlayerPrefs.GetString("asynclevel");
+			StartCoroutine("StartLoadingLevel");
+		}
+		else
+		{
+			Debug.LogError("Level not set using: asyncLevelToLoad(LEVELNAME)");
+		}
+
+
+	}
+	
+	IEnumerator StartLoadingLevel() {
+		AsyncOperation async = Application.LoadLevelAsync(levelToLoad);
+		yield return async;
+		Debug.Log("Loading complete");
+		if(async.isDone)
+		{
+			completedLevelLoad();
+		}
+	}
+
+
+	public static void asyncLevelToLoad(string _level)
+	{
+		PlayerPrefs.SetString("asynclevel", _level);
+	}
+
+	private void completedLevelLoad()
+	{
+
+		Debug.Log("Done");
+	}
+}
