797b3567e231c5c43d1fa9158b0b5aabdbc8b333
Hollie Buckets <bucketsinmn@gmail.com>
2015-02-06 20:00:15 -0500

    Setting up Text for story scene

--- Assets/Scripts/StoryScripts/StoryTextBehaviour.cs
+++ Assets/Scripts/StoryScripts/StoryTextBehaviour.cs
@@ -8,26 +8,31 @@ public class StoryTextBehaviour : MonoBehaviour {
 	public float scale = 10.0f;
 	public float speed = 1.0f;
 
-	private Text storyText;
+	private TextMesh storyText;
 	private string storyString = "This is the text in the story. It is pretty cool!";
 	private char[] storyChar;
+	private int storyCharLength;
 
 	// Use this for initialization
 	void Start () 
 	{
 		Debug.Log ("We've started");
-		storyText = GetComponent<Text>();
-		Debug.Log ("The value of storyText is: " + storyText.text);
+		storyText = GetComponent<TextMesh>();
 		storyChar = storyString.ToCharArray();
+		storyCharLength = storyChar.Length;
+
+		Debug.Log("The length of storyChar is: " + storyChar.Length);
+		PrintStory();
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void PrintStory () 
 	{
-		for( int i = 0; i < storyChar.Length; i++)
+		for( int i = 0; i < storyCharLength; i++)
 		{
-			//storyText.GetComponent<TextGenerator>() .Populate(storyChar[i]);
+			storyText.text += storyChar[i];
 			Debug.Log ("This is i: " + storyChar[i]);
 		}
+
 	}
 }
