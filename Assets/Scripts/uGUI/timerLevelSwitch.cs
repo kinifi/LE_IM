5b3c52a4a95fed63e82bc033967065b5e882ad08
kinifi <chrisfigueroa16@gmail.com>
2015-02-06 19:54:20 -0500

    Converted Logo Scene to Unity GUI

---
+++ Assets/Scripts/uGUI/timerLevelSwitch.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class timerLevelSwitch : MonoBehaviour {
+
+	public string levelNameToLoad;
+	public float m_Timer = 1.0f;
+	private bool m_timerDone = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(!m_timerDone)
+		{
+			m_Timer -= Time.deltaTime;
+		
+			if(m_Timer < 0)
+			{
+				m_timerDone = false;
+				onTimerComplete();
+			}
+		}
+	}
+
+	private void onTimerComplete()
+	{
+		Debug.Log("Timer Completed");
+		Application.LoadLevel(levelNameToLoad);
+	}
+}
