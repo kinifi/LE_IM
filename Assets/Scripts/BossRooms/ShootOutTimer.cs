558e1fcdaa001fd821177acbfe735a9d61b30c84
Hollie Buckets <bucketsinmn@gmail.com>
2015-02-07 20:31:54 -0500

    Set Up Shoot Out Boss
    Shoot Out "Boss" set up

---
+++ Assets/Scripts/BossRooms/ShootOutTimer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShootOutTimer : MonoBehaviour {
+
+
+	//Text configs
+	Text timerText;
+
+	// Use this for initialization
+	void Start () 
+	{
+		timerText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
