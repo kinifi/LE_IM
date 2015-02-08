7cd34863b7b025b7ff82ac76d887711b6d345cd7
kinifi <chrisfigueroa16@gmail.com>
2015-02-07 19:27:26 -0500

    Fixing Pause Menu

--- Assets/Scripts/uGUI/MegaMenu.cs
+++ Assets/Scripts/uGUI/MegaMenu.cs
@@ -5,8 +5,15 @@ using System.Collections;
 
 public class MegaMenu : MonoBehaviour {
 
-	public GameObject GameplayPanel, SoundPanel, VideoPanel;
+	//Panels to Toggle
+	public GameObject GameplayPanel, SoundPanel, VideoPanel, LearnPanel;
+	//The Regenerate Button reference in case this is a challenge level
+	public GameObject RegenerateButton;
+	//control panel are the top three buttons in the menu
 	public GameObject ControlPanel;
+	//Use this to toggle if this pause menu is in the challenge levels or not
+	public bool isChallengeLevels = false;
+	//is the pause Menu open?
 	private bool isOpen = false;
 
 
@@ -17,6 +24,7 @@ public class MegaMenu : MonoBehaviour {
 
 	void Update() {
 
+		//check for input
 		if(Input.GetButtonDown("Back"))
 		{
 
@@ -34,39 +42,34 @@ public class MegaMenu : MonoBehaviour {
 
 	}
 
-	/*
-	static GameObject FindFirstEnabledSelectable (GameObject gameObject)
-	{
-		GameObject go = null;
-		var selectables = gameObject.GetComponentsInChildren<Selectable> (true);
-		foreach (var selectable in selectables) {
-			if (selectable.IsActive () && selectable.IsInteractable ()) {
-				go = selectable.gameObject;
-				break;
-			}
-		}
-		return go;
-	}
-	*/
-
+	/// <summary>
+	/// Quits the game.
+	/// </summary>
 	public void QuitGame()
 	{
 		Application.Quit();
 	}
 
+	/// <summary>
+	/// Quits to main menu.
+	/// </summary>
 	public void quitToMainMenu()
 	{
 		Application.LoadLevel("MainMenu");
 	}
 
+	/// <summary>
+	/// Resets the level.
+	/// </summary>
 	public void resetLevel()
 	{
 		Application.LoadLevel(Application.loadedLevelName);
 	}
 
+	/// <summary>
+	/// Closes the menu.
+	/// </summary>
 	public void closeMenu() {
-
-
 
 		ControlPanel.SetActive(false);
 		GameplayPanel.SetActive(false);
@@ -74,11 +77,21 @@ public class MegaMenu : MonoBehaviour {
 		VideoPanel.SetActive(false);
 	}
 
+	/// <summary>
+	/// Setups the menu makes the Pause menu show in a default state.
+	/// Call this if wanting the Pause Menu to Display from anywhere
+	/// </summary>
 	public void setupMenu() {
+		isOpen = true;
 		ControlPanel.SetActive(true);
 		toggleGameplay();
+		//Disables the Regnerate button if on Challenge Levels
+		DisableRegenerate ();
 	}
 
+	/// <summary>
+	/// Toggles the gameplay and disables all the others
+	/// </summary>
 	public void toggleGameplay () {
 
 		GameplayPanel.SetActive(true);
@@ -87,6 +100,19 @@ public class MegaMenu : MonoBehaviour {
 		setFirstSelected.setFirstSelectedItem(GameObject.Find("Gameplay_Button"));
 	}
 
+	public void toggleLearnPanel()
+	{
+		isOpen = true;
+		ControlPanel.SetActive(false);
+		GameplayPanel.SetActive(false);
+		SoundPanel.SetActive(false);
+		VideoPanel.SetActive(false);
+		LearnPanel.SetActive(true);
+	}
+
+	/// <summary>
+	/// Toggles the sound panel and disables all the others
+	/// </summary>
 	public void toggleSoundPanel () {
 
 		GameplayPanel.SetActive(false);
@@ -95,11 +121,31 @@ public class MegaMenu : MonoBehaviour {
 
 	}
 
+	/// <summary>
+	/// Toggles the video panel and disables all the others
+	/// </summary>
 	public void toggleVideoPanel () {
 
 		GameplayPanel.SetActive(false);
 		SoundPanel.SetActive(false);
 		VideoPanel.SetActive(true);
+	}
+	
+	/// <summary>
+	/// Disables the Regnerate button if on Challenge Levels
+	/// </summary>
+	private void DisableRegenerate ()
+	{
+		if(isChallengeLevels == true)
+		{
+			RegenerateButton.SetActive(false);
+			Debug.Log ("Regenerate has been deactivated");
+		}
+		else
+		{
+			RegenerateButton.SetActive(true);
+			Debug.Log ("No need to deactivate regenerate");
+		}
 	}
 
 }
